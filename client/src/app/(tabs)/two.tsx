7c3ef0b3a582ca41003cd7dd9fa7e0221a7602d3
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-12-10 17:18:02 -0800

    Update two.tsx

--- client/src/app/(tabs)/two.tsx
+++ client/src/app/(tabs)/two.tsx
@@ -19,7 +19,10 @@ export default function TabTwoScreen() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://stockshub.duckdns.org:5001/fin");
+        // const response = await fetch("i");
+        const response = await fetch(
+          "http://stockshub.duckdns.org:5001/fin"
+        );
         // const response = await fetch(
         //   "https://stocks-backend-flask.vercel.app/fin"
         // );
