6ccd66f7150f8b5ae9aefdf450985bbb2f107113
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2025-01-29 19:31:39 -0800

    change to vercel backend

--- client/src/app/(tabs)/two.tsx
+++ client/src/app/(tabs)/two.tsx
@@ -20,12 +20,12 @@ export default function TabTwoScreen() {
     const fetchData = async () => {
       try {
         // const response = await fetch("i");
-        const response = await fetch(
-          "http://stockshub.duckdns.org:5001/fin"
-        );
         // const response = await fetch(
-        //   "https://stocks-backend-flask.vercel.app/fin"
+        //   "http://stockshub.duckdns.org:5001/fin"
         // );
+        const response = await fetch(
+          "https://stocks-backend-flask.vercel.app/fin"
+        );
 
         const data = await response.json();
         setStocks(data.daily_losers);
